1f4cda8658c2ca7b74b628f864acab2e156b2b17
Daniel Torres <torresestradadaniel@gmail.com>
2018-12-05 12:37:49 -0600

    i change Private entities

--- check.sh
+++ check.sh
@@ -13,6 +13,7 @@ curl -X GET http://0.0.0.0:3001/
 echo "Checking the KeyStone"
 curl -X GET http://0.0.0.0:5000/ -H 'Accept: application/json' -H 'X-Auth-token: ADMIN'
 echo "Creating Device Subscriptions to QL"
+
 curl -iX POST http://0.0.0.0:1026/v2/subscriptions -d @Subscriptions/DeviceToQL.json --header "Content-Type: application/json"
 echo "Creating Alert Subscriptions to QL"
 curl -iX POST http://0.0.0.0:1026/v2/subscriptions -d @Subscriptions/AlertToQL.json --header "Content-Type: application/json"
@@ -21,10 +22,10 @@ curl -iX POST http://0.0.0.0:1026/v2/subscriptions -d @Subscriptions/AlertToNoti
 echo "Creating Device Entity in Orion"
 curl -iX POST http://0.0.0.0:1026/v2/entities -d @"Orion Entities/Device.json" --header "Content-Type: application/json"
 echo "Creating Device Token Entity in Driving Service"
-curl -iX POST http://0.0.0.0:4005/api/device/token -d @"Datamodels Entities/DeviceToken.json" --header "Content-Type: application/json"
+curl -iX POST http://0.0.0.0:4005/api/device/token -d @"Private Entities/DeviceToken.json" --header "Content-Type: application/json"
 echo "Creating Zone Entity in Driving Service"
-curl -iX POST http://0.0.0.0:4005/api/zone  -d @"Datamodels Entities/Zone.json" --header "Content-Type: application/json"
+curl -iX POST http://0.0.0.0:4005/api/zone  -d @"Private Entities/Zone.json" --header "Content-Type: application/json"
 echo "Creating User Entity in Driving Service"
-curl -iX POST http://0.0.0.0:4005/api/user  -d @"Datamodels Entities/User.json" --header "Content-Type: application/json"
+curl -iX POST http://0.0.0.0:4005/api/user  -d @"Private Entities/User.json" --header "Content-Type: application/json"
 echo "Creating Alert"
 curl -iX POST http://0.0.0.0:1026/v2/entities -d @"Orion Entities/Alert.json" --header "Content-Type: application/json"
